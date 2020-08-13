42c098b3ec2a86970f69fcf5625fc972eb03fe63
zoha131 <abirhasanzoha@gmail.com>
2020-08-13 10:12:57 +0600

    Add static data

---
+++ SmartPDFTools/SmartPDFTools/Model/Extension/String+Extension.swift
@@ -0,0 +1,42 @@
+//
+//  String+Extension.swift
+//  SmartPDFTools
+//
+//  Created by Zoha on 8/13/20.
+//  Copyright © 2020 Zoha. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+  static let bmpTopdf = "BMP to PDF"
+  static let bmpTotxt = "BMP to TXT"
+  static let compresspdf = "Compress PDF"
+  static let decryptpdf = "Decrypt PDF"
+  static let encryptpdf = "Encrypt PDF"
+  static let excelTopdf = "Excel to PDF"
+  static let extractImages = "Extract Images"
+  static let htmlTopdf = "HTML to PDF"
+  static let jpgTotxt = "JPG to TXT"
+  static let jpgTopdf = "JPG to PDF"
+  static let mergpdf = "Merge PDF"
+  static let pdfTobmp = "PDF to BMP"
+  static let pdfTohtml = "PDF to HTML"
+  static let pdfTojpg = "PDF to JPG"
+  static let pdfTopng = "PDF to PNG"
+  static let pdfToppt = "PDF to PPT"
+  static let pdfTops = "PDF to PS"
+  static let pdfTotiff = "PDF to TIFF"
+  static let pdfTotxt = "PDF to TXT"
+  static let pdfToword = "PDF to Word"
+  static let pngTopdf = "PNG to PDF"
+  static let pngTotxt = "PNG to TXT"
+  static let pptTopdf = "PPT to PDF"
+  static let rotatepdf = "Rotate PDF"
+  static let splitpdf = "Split PDF"
+  static let tiffTopdf = "TIFF to PDF"
+  static let tiffTotxt = "TIFF to TXt"
+  static let txtTopdf = "TXT to PDF"
+  static let watermarkpdf = "Watermark PDF"
+  static let wordTopdf = "Word to PDF"
+}
---
+++ SmartPDFTools/SmartPDFTools/Model/Extension/UIColor+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extension.swift
+//  SmartPDFTools
+//
+//  Created by Zoha on 8/13/20.
+//  Copyright © 2020 Zoha. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  static let bmpTopdf = UIColor(named: "BMP to PDF")!
+}
+
